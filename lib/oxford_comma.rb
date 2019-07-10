72bbceb286b4328b548948c6cfd72fb9642cf015
Celal Cankaya <celalcankaya@outlook.com>
2019-07-10 21:29:08 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    return array[0]
+  end
+  if array.size == 2
+    return array[0] + " and " +array[1]
+  end
+  if array.size > 2
+    array[-1] = "and " +array[-1]
+    return array.join(", ")
+  end
 end
