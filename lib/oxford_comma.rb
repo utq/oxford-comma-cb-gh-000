e936d3f1616c161637eb498ae553e095b1b88ea9
Celal Cankaya <celalcankaya@outlook.com>
2019-07-10 21:28:54 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
     return array[0]
   end
   if array.size == 2
-    return array[0] + "and " +array[1]
+    return array[0] + " and " +array[1]
   end
   if array.size > 2
     array[-1] = "and " +array[-1]
